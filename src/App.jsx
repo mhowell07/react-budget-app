922588f9d7dc16a00ef7dcc38f6475ac4bda1120
Matt Howell <matthew.howell07@gmail.com>
2022-11-26 19:33:22 -0600

    budget card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-import { useState } from 'react'
+// library imports
+import { Button, Stack } from 'react-bootstrap'
+import Container from 'react-bootstrap/Container'
+
+// custom components
+import BudgetCard from './components/BudgetCard'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <div className="App">
-      
+      <Container className='my-4'>
+        <Stack direction="horizontal" gap="2" className="mb-4">
+          <h1 className="me-auto">Budgets</h1>
+          <Button variant="primary" className="" >Add Budget</Button>
+          <Button variant="outline-primary" className="" >Add Expense</Button>
+        </Stack>
+        <div style={{ display: "grid", gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))", gap: "1rem", alignItems: "flex-start"}}>
+          <BudgetCard 
+            name="Entertainment" 
+            amount={1200} 
+            max={1000}
+            gray
+          ></BudgetCard>
+        </div>
+      </Container>
     </div>
   )
 }
